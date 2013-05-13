3c93b95af69eca70f8e81855a8a9b28576c440bd
Michael Elfassy <michael@smashingboxes.com>
2013-05-13 16:20:32 -0400

    updated jquery

--- src/jquery.livesearch.js
+++ src/jquery.livesearch.js
@@ -39,9 +39,9 @@
       };
     }
 
-    $(this).live('keypress', perform);
-    $(this).live('keydown', perform);
-    $(this).live('keyup', perform);
-    $(this).bind('blur', perform);
+    $(this).on('keypress', perform);
+    $(this).on('keydown', perform);
+    $(this).on('keyup', perform);
+    $(this).on('blur', perform);
   }
 })(jQuery);
